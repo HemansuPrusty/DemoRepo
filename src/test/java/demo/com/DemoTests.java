5bf0877a3eef778be023996a372a28717edb35b6
hemansu <hemansu.prusty@gmail.com>
2018-07-31 16:52:53 +0530

    selenium demo

---
+++ src/test/java/demo/com/DemoTests.java
@@ -0,0 +1,23 @@
+package demo.com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTests {
+	
+	@Test
+	public void guiTitleVerify(){
+		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\geckodriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.toolsqa.com");
+		String st = driver.findElement(By.xpath("//span[text()='HOME']")).getText();
+		System.out.println("************Expected Title****************  :" +st);
+		Assert.assertEquals(st, "HOME");
+		driver.close();
+	}
+
+}
